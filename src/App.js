13abb6fbaf413e576684a3faa0cd88db1f09e1c7
Shahmeer236 <shahmeer.rizwan.236@gmail.com>
2023-04-01 17:42:30 +0500

    last

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
      <div className='container mx-auto px-2 max-w-5xl pt-10 md:pt-32'>
       <h1 className='text-center font-bold text-2xl text-gray-700'>CRUD with Redux toolkit</h1> 
-           <AddUser/>
+           {/* <AddUser/> */}
       <Routes>
         <Route path="/" element={ <UserList/>}/>
         <Route path='/add-user' element={<AddUser/>} />
